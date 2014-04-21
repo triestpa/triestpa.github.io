9c0be6468ca29b69c376922760330c53ba83742b
triestpa <triestpa@grinnell.edu>
2014-04-21 13:46:09 -0500

    add popover on voyse source code

--- js/scripts.js
+++ js/scripts.js
@@ -1,101 +1,101 @@
-				$("#nav ul li a").on('click', function(e) {
+$("#nav ul li a").on('click', function(e) {
 
-				// prevent default anchor click behavior
-				e.preventDefault();
+	// prevent default anchor click behavior
+	e.preventDefault();
 
-				// store hash
-				var hash = this.hash;
+	// store hash
+	var hash = this.hash;
 
-				// animate
-				$('html, body').animate({
-					scrollTop: $(this.hash).offset().top
-				}, 300, function(){
+	// animate
+	$('html, body').animate({
+		scrollTop: $(this.hash).offset().top
+	}, 300, function(){
 
-				   // when done, add hash to url
-				   // (default click behaviour)
-				   window.location.hash = hash;
-				});
+	   // when done, add hash to url
+	   // (default click behaviour)
+	   window.location.hash = hash;
+	});
 
-			});
+});
 
-				var jumboHeight = $('.jumbotron').outerHeight();
-				function parallax(){
-					var scrolled = $(window).scrollTop();
-					$('.bg').css('height', (jumboHeight-scrolled) + 'px');
-				}
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+	var scrolled = $(window).scrollTop();
+	$('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
 
-				$(window).scroll(function(e){
-					parallax();
-				});
+$(window).scroll(function(e){
+	parallax();
+});
 
-				$("#profilepic").hover(function(){
-					$("#profilepic").addClass("animated rubberBand");
-				},function(){
-					$("#profilepic").removeClass("animated rubberBand");
-				});
+$("#voyse-code").tooltip();
 
-				$("#voyse-pic").hover(function(){
-					$("#voyse-pic").addClass("animated pulse");
-				},function(){
-					$("#voyse-pic").removeClass("animated pulse");
-				});
+$("#profilepic").hover(function(){
+	$("#profilepic").addClass("animated rubberBand");
+},function(){
+	$("#profilepic").removeClass("animated rubberBand");
+});
 
-				$("#appdev-pic").hover(function(){
-					$("#appdev-pic").addClass("animated pulse");
-				},function(){
-					$("#appdev-pic").removeClass("animated pulse");
-				});
+$("#voyse-pic").hover(function(){
+	$("#voyse-pic").addClass("animated pulse");
+},function(){
+	$("#voyse-pic").removeClass("animated pulse");
+});
 
-				$("#email").mouseover(function() {
-					$(this).text('patrick.triest@gmail.com');
-				});
-				
-				function transition(image) {
-					$('#apppic').addClass('animated bounceOutRight');
-					if ($('#apppic').attr("src") == image){
-						$('#apppic').removeClass('animated bounceOutRight');
-						$('#apppic').addClass('animated bounceInRight');
-					}
-					$('#apppic').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-						$('#apppic').attr("src", image);
-					});
-				}
+$("#appdev-pic").hover(function(){
+	$("#appdev-pic").addClass("animated pulse");
+},function(){
+	$("#appdev-pic").removeClass("animated pulse");
+});
 
-				$('#apppic').load(function(){
-					$('#apppic').removeClass('animated bounceOutRight');
-					$('#apppic').addClass('animated bounceInRight');
-				});
+$("#email").mouseover(function() {
+	$(this).text('patrick.triest@gmail.com');
+});
 
+function transition(image) {
+	$('#apppic').addClass('animated bounceOutRight');
+	if ($('#apppic').attr("src") == image){
+		$('#apppic').removeClass('animated bounceOutRight');
+		$('#apppic').addClass('animated bounceInRight');
+	}
+	$('#apppic').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+		$('#apppic').attr("src", image);
+	});
+}
 
-				
-				$(function(){
+$('#apppic').load(function(){
+	$('#apppic').removeClass('animated bounceOutRight');
+	$('#apppic').addClass('animated bounceInRight');
+});
 
-					$('#sandb').on('shown.bs.collapse', function () {
-						transition("img/sandb.png");
-					});
+$(function(){
 
-					$('#kdic').on('shown.bs.collapse', function () {
-						transition("img/kdic.png");
-					});    
+	$('#sandb').on('shown.bs.collapse', function () {
+		transition("img/sandb.png");
+	});
 
-					$('#db').on('shown.bs.collapse', function () {
-						transition("img/db.png");
-					});  
+	$('#kdic').on('shown.bs.collapse', function () {
+		transition("img/kdic.png");
+	});    
 
-					$('#glicious').on('shown.bs.collapse', function () {
-						transition("img/glicious.png");
-					}); 
+	$('#db').on('shown.bs.collapse', function () {
+		transition("img/db.png");
+	});  
 
-					$('#friendlyearth').on('shown.bs.collapse', function () {
-						transition("img/friendlyearth.png");
-					});
+	$('#glicious').on('shown.bs.collapse', function () {
+		transition("img/glicious.png");
+	}); 
 
-					$('#voyse').on('shown.bs.collapse', function () {
-						transition("img/voyse.png");
-					});
+	$('#friendlyearth').on('shown.bs.collapse', function () {
+		transition("img/friendlyearth.png");
+	});
 
-					$('#facetag').on('shown.bs.collapse', function () {
-						transition("img/facetag.png");
-					});
+	$('#voyse').on('shown.bs.collapse', function () {
+		transition("img/voyse.png");
+	});
 
-				});
+	$('#facetag').on('shown.bs.collapse', function () {
+		transition("img/facetag.png");
+	});
+
+});
